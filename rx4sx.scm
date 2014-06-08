6083351d2f76d9743f26e071c6e8dbcbd8cd5a6f
hnagamin <1995.hnagamin@gmail.com>
2014-06-08 19:20:18 +0900

    use more procedures
    Now that states, alphabets, and sets of accept state must be
    1-parameter functions.
    
    (membership SET)
      SET: set
      --
      membership returns 1-parameter function f(x).
      f(x) = (x is an element of SET).
    
    (cons-membership a f)
      a: S expression
      f: 1-parameter function
      --
      cons-membership returns 1-parameter function g(x).
      g(x) = (x = a) || f(x).
    
    (append-membership a b)
      a: 1-parameter function
      b: 1-parameter function
      --
      append-membership returns 1-parameter function f(x).
      f(x) = a(x) || b(x).
    
    rename:
    (stacpt M) -> (accept M)

--- rx4sx.scm
+++ rx4sx.scm
@@ -4,25 +4,34 @@
     ((equal? a (car (car rel))) (cadr (car rel)))
     (else (rel-apply a (cdr rel)))))
 
+(define (cons-membership a b)
+  (let ((A (if (procedure? a) a (lambda (x) (equal? x a))))
+        (B (if (procedure? b) b (lambda (x) (equal? x b)))))
+    (lambda (x) (or (A x) (B x)))))
+
 (define (make-e-fun transit e-transit)
   (lambda (state . char)
     (if (null? char)
       (rel-apply state e-transit)
       (rel-apply (list state (car char)) transit))))
 
+(define (membership set)
+  (lambda (a)
+    (member a set)))
+
 (define (make-nfa Q S T q0 F) (list Q S T q0 F))
 (define (state M)      (car M))
 (define (alphabet M)   (car (cdr M)))
 (define (transition M) (car (cdr (cdr M))))
 (define (start M)      (car (cdr (cdr (cdr M)))))
-(define (stacpt M)     (car (cdr (cdr (cdr (cdr M))))))
+(define (accept M)     (car (cdr (cdr (cdr (cdr M))))))
 
 (define (transit M r . a)
   (if (null? a)
     ((transition M) r)
     ((transition M) r (car a))))
 
-(define (accept-state? M a) (member a (stacpt M)))
+(define (accept-state? M a) ((accept M) a))
 
 (define (make-configration M state str) (list M state str))
 (define (conf-machine conf) (car conf))
@@ -44,11 +53,11 @@
         (str (conf-string conf)))
     (append
       (map (lambda (state)
-             (make-configration M state str))
-           (transit M r))
-      (map (lambda (state)
              (make-configration M state (cdr str)))
-           (transit M r (car str))))))
+           (transit M r (car str)))
+      (map (lambda (state)
+             (make-configration M state str))
+           (transit M r)))))
 
 (define (conf-accepts? conf)
   (if (empty-string-conf? conf)
@@ -68,7 +77,7 @@
 
 (define (new-state state)
   (letrec ((A (lambda (n)
-                (if (member n state)
+                (if (state n)
                   (A (+ 1 n))
                   n))))
     (A 0)))
@@ -84,15 +93,28 @@
                                       (cons (start M) (transit q))
                                       (transit q (car a))))
                (else (if (null? a) (transit q) (transit q (car a))))))))
-    (make-nfa (cons q0 (state M)) (alphabet M) delta q0 (cons q0 (stacpt M)))))
+    (make-nfa (cons-membership q0 (state M))
+              (alphabet M)
+              delta
+              q0
+              (cons-membership q0 (accept M)))))
+
+;(define (sum M1 M2)
+;  (let ((delta (lambda (q . a)
+;                 (if (null? a)
+;                   (cons ((transition M1) (car q))
+;                         ((transition M2) (cdr q)))
+;                   (cons ((transition M1) (car q) a)
+;                         ((transition M2) (car q) a))))))
+;    (make-nfa (cons (start M1) (start
 
 (define l '(((0 #\0) (1))
             ((1 #\0) (2))
             ((2 #\1) (3))
             ))
-(define M (make-nfa '(0 1 2 3)
-                    '(#\0 #\1)
+(define M (make-nfa (membership '(0 1 2 3))
+                    (membership '(#\0 #\1))
                     (make-e-fun l '())
                     0
-                    '(3)))
+                    (membership '(3))))
 (define N (star M))
